bcb4276c0fea6c3ff0c9d4c05704554da86d3d80
note <omeraligencoglu7@gmail.com>
2023-05-26 14:02:15 -0400

    add behaviour tree namespace components

---
+++ Assets/Code/Scripts/BehaviourTree/Sequence.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+
+namespace BehaviourTree
+    // behaviour tree template adapted from https://www.youtube.com/watch?v=aR6wt5BlE-E
+{
+    public class Sequence : BehaviourNode
+        // Node that returns success when all its immediate children return success, and returns failure otherwise.
+        // Acts like an AND gate.
+    {
+        public Sequence() : base() { }
+        public Sequence(List<BehaviourNode> children) : base(children) { }
+
+        public override NodeState _Evaluate()
+        {
+            bool anyChildIsRunning = false;
+
+            foreach (BehaviourNode child in children)
+            {
+                switch (child._Evaluate())
+                {
+                    case NodeState.FAILURE:
+                        state = NodeState.FAILURE;
+                        return state;
+                    case NodeState.SUCCESS:
+                        continue;
+                    case NodeState.RUNNING:
+                        anyChildIsRunning = true;
+                        continue;
+                    default:
+                        state = NodeState.SUCCESS;
+                        return state;
+                }
+            }
+
+            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            return state;
+        }
+    }
+}
+
