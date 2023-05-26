834447090f01d713be9cd48ff180be81e84d6a44
Oktay Comu <oktaycomu@192.168.1.89>
2023-05-26 23:00:31 +0300

    feat: eye sight scripts and eye target

--- Assets/Code/Scripts/Eye/EyeSight.cs
+++ Assets/Code/Scripts/Eye/EyeSight.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EyeSight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EyeTarget EyeTarget;
+    public int RaycastIgnoreLayer = 6;
+
+    private void FixedUpdate()
     {
-        
+        if (IsTargetInSight())
+        {
+            EyeTarget.ReceiveDamage();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsTargetInSight()
     {
+        var origin = transform.position;
+        var targetPosition = EyeTarget.GetTargetPosition();
+        var direction = targetPosition - origin;
+        var layerMask = ~(1 << RaycastIgnoreLayer);
         
+        // Draw debug ray
+        Debug.DrawLine(origin, targetPosition, Color.magenta);
+        
+        // Cast ray
+        var hit = Physics.Raycast(origin, direction, out RaycastHit hitInfo, 1000, layerMask);
+            
+        // Return true IFF hitting the target collider
+        return hit && hitInfo.collider == EyeTarget.GetTargetCollider();
     }
 }
