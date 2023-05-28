3155f353b911fcad6625d1c0d6e1e45657b4f3c7
RoganGutwillinger <rmgutwillinger@gmail.com>
2023-05-28 13:20:06 -0400

    Stopped using DOTween for continuous shake

--- Assets/Code/Scripts/Camera/CinemachineShake.cs
+++ Assets/Code/Scripts/Camera/CinemachineShake.cs
@@ -8,10 +8,24 @@ public class CinemachineShake : MonoBehaviour
 {
   public static CinemachineShake Instance { get; private set; }
 
+  private const float attackShakeMaxIntensity = 0.8f;
+  private const float attackShakeTransitionSpeed = 0.5f;
+
   [SerializeField] private CinemachineVirtualCamera virtualCamera;
   private CinemachineBasicMultiChannelPerlin perlin;
+  private bool underAttack;
+  private float currentShakeAmplitude {
+    get { return this.perlin.m_AmplitudeGain; }
+    set { this.perlin.m_AmplitudeGain = value; }
+  }
 
-  private bool on;
+  public void StartAttackShake() {
+    this.underAttack = true;
+  }
+
+  public void StopAttackShake() {
+    this.underAttack = false;
+  }
 
   private void Awake() {
     Instance = this;
@@ -19,14 +33,31 @@ public class CinemachineShake : MonoBehaviour
 
   private void Start() {
     this.perlin = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    this.underAttack = false;
   }
 
-  public void DoSmoothCameraShake(float intensity, float transitionTime) {
-    DOTween.To(() => this.perlin.m_AmplitudeGain, x => this.perlin.m_AmplitudeGain = x, intensity, transitionTime);
+  // Deprecated
+  private void DoSmoothCameraShake(float intensity, float transitionTime, TweenCallback callback) {
+    DOTween.To(() => this.currentShakeAmplitude, x => this.currentShakeAmplitude = x, intensity, transitionTime).OnComplete(callback);
   }
 
-  public void DoCameraShakeImpulse(float intensity, float duration) {
+  // Deprecated
+  private void DoCameraShakeImpulse(float intensity, float duration) {
     this.perlin.m_AmplitudeGain = intensity;
-    this.DoSmoothCameraShake(0f, duration);
+    this.DoSmoothCameraShake(0f, duration, null);
+  }
+
+  private void Update() {
+    // This is a bit of a messy implementation, DOTween was causing issues. May revisit!
+    if (this.underAttack) {
+      if (this.currentShakeAmplitude < attackShakeMaxIntensity) {
+        this.currentShakeAmplitude = Mathf.Min(attackShakeMaxIntensity, this.currentShakeAmplitude + Time.deltaTime * attackShakeTransitionSpeed);
+      }
+    }
+    else {
+      if (this.currentShakeAmplitude > 0) {
+        this.currentShakeAmplitude = Mathf.Max(0, this.currentShakeAmplitude - Time.deltaTime * attackShakeTransitionSpeed);
+      }
+    }
   }
 }
